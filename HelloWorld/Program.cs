8740e36962ef36b634810f61af7ae7e322f6924e
Andy Gomez <andygomez4232@gmail.com>
2026-09-09 11:19:45 -0400

    Add header comment and introduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Name: Andy Gomez
+* Course: CSCI 1250, Section 001
+* Assignment: LAb 0, Hello World
+* Date: September 9, 2026
+* Description: Prints a greeting and short introduction to the console
+*/
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Andy Gomez.");
+Console.WriteLine("This is my first program at ETSU.");
