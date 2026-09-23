ecbedbddfb4d3ede3a9c8632c6bc3be29278e7a6
Andy Gomez <andygomez4232@gmail.com>
2026-09-23 10:48:08 -0400

    Asks how many miles traveled

---
+++ lab02-trip/TripCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Name:    Andy Gomez
+ * Course:  CSCI 1250, Section 001
+ * Assignment:  Lab 02, Trip Calculator
+ * Date:    September 22, 2026
+ * Description: Calculates the fuel, food, and work behind one road trip.
+*/
+
+Console.Write("How many miles did you travel? ");
+int roundTrip = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("What is the mile per gallon for the car you travel on? ");
+double milePerGallon = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("What was the average price per gallon you paid? ");
+double pricePerGallon = Convert.ToDouble(Console.ReadLine());
+
+double gallonsNeeded = roundTrip / milePerGallon;
+
+double fuelcost = gallonsNeeded * pricePerGallon;
+
+Console.WriteLine ("Gallons needed: " + gallonsNeeded.ToString("F2"));
+Console.WriteLine ("Fuel cost: " + fuelcost.ToString("C"));
+
+Console.Write ("How many people are going? ");
